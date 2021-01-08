e5c75dde24cb1bfaec13034b25afc4bdcaf01c02
joy.smile <joy@joysmileui-MacBookPro.local>
2021-01-08 13:07:58 +0900

    refactor: movable method

--- src/main/java/Car/CarManager.java
+++ src/main/java/Car/CarManager.java
@@ -7,9 +7,6 @@ public class CarManager {
     private static final int RANDON_BOUND = 10;
 
     public boolean movable() {
-        if (NumberUtil.generateRandomNumber(RANDON_BOUND) >= MOVE_CONDITION_THRESHOLD)
-            return true;
-
-        return false;
+        return NumberUtil.generateRandomNumber(RANDON_BOUND) >= MOVE_CONDITION_THRESHOLD;
     }
 }
